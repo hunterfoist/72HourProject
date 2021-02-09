6a546644d195f08d28764be8a6e0ad612a83a280
Hunter Foist <hfoist@gmail.com>
2021-02-09 17:38:59 -0500

    added more info on card

--- src/components/RestaurantChild.js
+++ src/components/RestaurantChild.js
@@ -11,8 +11,10 @@ const RestaurantChild = (props) => {
         
         <CardBody>
           <CardTitle tag="h3" className="text-dark">{props.restaurantChild.restaurant.name}</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">{props.restaurantChild.restaurant.location.locality_verbose}</CardSubtitle>
-          {/* <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText> */}
+          
+          <CardSubtitle tag="h4" className="mb-2 text-muted">{props.restaurantChild.restaurant.location.locality_verbose}</CardSubtitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">Cuisine: {props.restaurantChild.restaurant.cuisines}</CardSubtitle>
+          <br/>
           <Button color="secondary" size="lg" href={props.restaurantChild.restaurant.url} target="blank">More info about this restaurant</Button>
         </CardBody>
       </Card>
